7c6c063f57cc6ead9e5bacfa78a28ba7b0f87ba3
Thiago Paiva <thiagohdaqw@gmail.com>
2022-08-30 18:54:12 -0300

    ETAPA_2: Backend esperar o bancos de dados estar pronto
    Apos testes, foi constatado que na primeira vez que os containers sao iniciados
    acontece um erro no backend devido a problemas de conexao com o banco de dados.
    O que acontece é que, mesmo com o depends on, o backend inicia antes do banco de dados
    estar pronto para conexao.
    A solucao foi fazer um shell script que fica aguardando o banco de dados esta pronto
    pra receber conexao.
    A solucao baseada em: https://stackoverflow.com/a/50108745

--- library_back/start.sh
+++ library_back/start.sh
@@ -1,4 +1,14 @@
 #! /bin/bash
+
+function wait_for_db() {
+    while !</dev/tcp/$POSTGRES_HOST/$POSTGRES_PORT; do
+        echo "Waiting database start... Retrying in 3 secs...";
+        sleep 3;
+    done; 
+}
+
+wait_for_db
+
 python manage.py makemigrations
 python manage.py migrate
 python manage.py runserver 0.0.0.0:8000
