b933ea1edfd795e35940b38b55cf180ceed91b27
Thiago Paiva <thiagohdaqw@gmail.com>
2022-09-01 17:21:38 -0300

    ETAPA_6: CI teste frontend
    O teste do frontend estava falhando porque ele esperava uma palavra que nao existia mais, entao isso foi resolvido

--- library_front/src/App.test.js
+++ library_front/src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders welcome', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Bem vindo a Biblioteca/i);
   expect(linkElement).toBeInTheDocument();
 });
